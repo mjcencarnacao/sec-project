f1ab3188fa0f82dfc8ecb8c1e0271d636137a50e
Miguel Encarnação <mjceo@iscte-iul.pt>
2023-04-11 12:00:10 +0100

    [SEC-MERGED-TESTING] Added Imports and Flush

--- sec-project-client/src/main/java/com/sec/project/domain/usecases/CheckBalanceUseCase.java
+++ sec-project-client/src/main/java/com/sec/project/domain/usecases/CheckBalanceUseCase.java
@@ -1,6 +1,7 @@
 package com.sec.project.domain.usecases;
 
 import com.sec.project.domain.repositories.MessagingService;
+import com.sec.project.infrastructure.annotations.FlushUDPBuffer;
 import com.sec.project.models.enums.ReadType;
 import com.sec.project.models.records.Message;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +24,7 @@ public class CheckBalanceUseCase {
         this.messagingService = messagingService;
     }
 
+    @FlushUDPBuffer
     public void execute(int destination, ReadType readType) {
         Message balanceRequest = new Message(CHECK_BALANCE, -1, -1, String.valueOf(-1), destination, -1);
         messagingService.sendMessage(balanceRequest, readType);
